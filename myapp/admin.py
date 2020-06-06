6c79b167abaafe37cee5ec392219259d2ccd5c11
kaphie <bkafrika144@gmail.com>
2020-06-06 17:08:45 +0300

    adding content to admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Comment, Rate, Project
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Rate)
